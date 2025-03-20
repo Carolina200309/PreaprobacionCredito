93f8f2eb5feb7e105c2daba907e9755e4c839d3d
Juan Arango <100418277+JuanArango3@users.noreply.github.com>
2025-03-20 12:30:57 -0500

    Implementada logica de negocio y datos de prueba en memoria para pruebas unitarias

---
+++ CapaNegocio/PuntajeDTO.cs
@@ -0,0 +1,22 @@
+﻿namespace CapaNegocio
+{
+    public class SolicitudDTO
+    {
+        public SolicitudDTO()
+        {
+
+        }
+
+        public SolicitudDTO(string tipoDocumento, int numeroDocumento, int puntaje)
+        {
+            this.tipoDocumento = tipoDocumento;
+            this.numeroDocumento = numeroDocumento;
+            this.puntaje = puntaje;
+        }
+
+        public string tipoDocumento { get; set; }
+        public int numeroDocumento { get; set; }
+        public int puntaje { get; set; }
+    }
+
+}
